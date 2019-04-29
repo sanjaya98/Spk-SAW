3753b63e7bcf64edf452a39802d830409424f13f
Riyat sanjaya <47327703+sanjaya98@users.noreply.github.com>
2019-04-29 21:31:37 +0700

    css

---
+++ css/cetak.php
@@ -0,0 +1,19 @@
+.header, h2 {
+    color: rgb(7, 7, 7);
+    text-align: center;
+    font-family: Arial, Helvetica, sans-serif;
+}
+h3 {
+    text-align: center;
+    font-family: Arial, Helvetica, sans-serif;
+    margin-top: -20px;
+}
+hr {
+    width: 400px;
+    margin-bottom: 20px;
+    margin-top: -18px;
+}
+
+th {
+    color: black;
+}
