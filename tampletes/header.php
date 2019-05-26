a740bdffb6e76dbf043681f5148fcb02207be281
Riyat sanjaya <47327703+sanjaya98@users.noreply.github.com>
2019-05-26 11:28:12 +0700

    menambahkan folder tamplates dan menambahkan file

---
+++ tampletes/header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+
+     <!-- My CSS -->
+    <link rel="stylesheet" href="../css/style.css">
+    
+
+    <title>SPK-SAW</title>
+  </head>
+  <body>
+ 
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+            <a class="navbar-brand" href="index.php">SPK</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                <a class="nav-item nav-link" href="index.php">Home</a>
+                <a class="nav-item nav-link" href="input.php">Input</a>
+                <a class="nav-item nav-link" href="view.php">View</a>
+                <a class="nav-item nav-link" href="menghitungsaw.php">Menghitung SAW</a>
+                <a class="nav-item nav-link " href="logout.php"><i class="fas fa-sign-out-alt"></i> Logout</a>
+            </div>
+        </div>
+    </div>
+    </nav>
